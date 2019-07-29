a95584cb27c8e4e721e9d4bef778273ec8e51730
lichenguang@1357.cn <[email]>
2019-07-29 10:10:42 +0800

    项目初始化

--- src/vuex/mutationTypes.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by pc on 2018/6/19.
- */
-export const SET_NAME = 'SET_NAME'
-export const SET_LOGINMSG = 'SET_LOGINMSG'
-export const SET_COLOR = 'SET_COLOR'
-export const SET_ROUTE = 'SET_ROUTE'
-export const SET_OPENKEYS = 'SET_OPENKEYS'
